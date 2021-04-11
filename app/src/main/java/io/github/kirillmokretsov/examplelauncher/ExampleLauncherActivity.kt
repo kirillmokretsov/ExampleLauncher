77e75f1a1bf746a4209140d2a3a3d9eee475d1f9
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-11 12:46:41 +0300

    Create launcher activity

---
+++ app/src/main/java/io/github/kirillmokretsov/examplelauncher/ExampleLauncherActivity.kt
@@ -0,0 +1,11 @@
+package io.github.kirillmokretsov.examplelauncher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleLauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_launcher)
+    }
+}
