37f00b94e9e0873163fd9fb9c83047e5dc48be32
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-11 12:55:32 +0300

    Add simple code that inits recyclerView and logs amount of found activities

--- app/src/main/java/io/github/kirillmokretsov/examplelauncher/ExampleLauncherActivity.kt
+++ app/src/main/java/io/github/kirillmokretsov/examplelauncher/ExampleLauncherActivity.kt
@@ -1,11 +1,36 @@
 package io.github.kirillmokretsov.examplelauncher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "ExampleLauncherActivity"
 
 class ExampleLauncherActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_example_launcher)
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        setupAdapter()
+
+    }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+
+        Log.i(TAG, "Found ${activities.size}")
     }
 }
